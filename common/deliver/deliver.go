7ed7b9a5d0a005865c7a95d3d647ddf822dea40f
Yoav Tock <tock@il.ibm.com>
2019-10-03 14:54:05 +0300

    Byz pull: deliver header with sig
    Change-Id: I05156cad795f6045706a57a9a837a8bad41108fb
    Signed-off-by: Yoav Tock <tock@il.ibm.com>

--- common/deliver/deliver.go
+++ common/deliver/deliver.go
@@ -331,6 +331,10 @@ func (h *Handler) deliverBlocks(ctx context.Context, srv *Server, envelope *cb.E
 
 		logger.Debugf("[channel: %s] Delivering block for (%p) for %s", chdr.ChannelId, seekInfo, addr)
 
+		if seekInfo.ContentType == ab.SeekInfo_HEADER_WITH_SIG {
+			block.Data = nil
+		}
+
 		if err := srv.SendBlockResponse(block); err != nil {
 			logger.Warningf("[channel: %s] Error sending to %s: %s", chdr.ChannelId, addr, err)
 			return cb.Status_INTERNAL_SERVER_ERROR, err
